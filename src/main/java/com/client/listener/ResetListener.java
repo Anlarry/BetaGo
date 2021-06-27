4017d266b1567ae9bc56e6c163f20fd0bd64357f
Anlarry <anlarry@foxmail.com>
2021-06-27 09:14:49 +0800

    :tada: first commit

---
+++ src/main/java/com/client/listener/ResetListener.java
@@ -0,0 +1,21 @@
+package com.client.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import com.client.gamedata.GameData;
+import com.client.ui.ChessBoard;
+/**
+ * 重来按钮的监听事件
+ */
+public class ResetListener implements ActionListener{
+
+	public void actionPerformed(ActionEvent e) {
+		GameData.Reset();
+		JOptionPane.showConfirmDialog(null, "现在可以重新和其他玩家游玩啦！", "已经和对方断开连接", JOptionPane.DEFAULT_OPTION,
+				JOptionPane.DEFAULT_OPTION);
+	}
+	
+}
