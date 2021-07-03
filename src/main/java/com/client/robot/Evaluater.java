12986d06cdd15c9ac4016e8a3b16ebf91fc5c7af
Anlarry <anlarry@foxmail.com>
2021-07-03 15:39:24 +0800

    finish alpha beta pruning

--- src/main/java/com/client/robot/Evaluater.java
+++ src/main/java/com/client/robot/Evaluater.java
@@ -6,16 +6,21 @@ import com.client.ui.ChessPoint;
 import java.awt.Color;
 import java.util.concurrent.ExecutionException;
 
+import javax.jws.Oneway;
+
 
 public class Evaluater {
+    static public int INF = 1000000;
     private final int FIVE = 10000;
     private final int FOUR = 1000;
     private final int semiFOUR = 100;
     private final int THREE = 100;
     private final int semiTHREE = 10;
     private final int TWO = 10;
-    private final int semiTWO = 1;
+    private final int semiTWO = 5;
     private final int ONE = 1;
+    private final int semiONE = 0;
+    private final int DISTENSE_DECEY = 1;
 
     private int value = 0;
     private Color color;
@@ -40,31 +45,40 @@ public class Evaluater {
     public void step(int x, int y, Color color) {
         int tmpVal = evalRow(x) + evalCol(y) + evalDiag(x-y) + evalAntiDiag(x+y);
         value -= tmpVal;
+        int decay = evalDistense(x, y);
+        if(this.color == color) {
+            decay = -decay;
+        }
+        value += decay;
         cp[x][y] = new ChessPoint(x, y, color);
         value += evalRow(x) + evalCol(y) + evalDiag(x-y) + evalAntiDiag(x+y);
     }
     public void unstep(int x, int y) {
         int tmpVal = evalRow(x) + evalCol(y) + evalDiag(x-y) + evalAntiDiag(x+y);
         value -= tmpVal;
+        Color color = cp[x][y].getCPColor();
         cp[x][y] = null;
+        int decay = evalDistense(x, y);
+        if(this.color == color)  decay = -decay;
+        value -= decay;
         value += evalRow(x) + evalCol(y) + evalDiag(x-y) + evalAntiDiag(x+y);
     }
     private int evalRow(int r) {
-        return evalLine(r, 0, 0, 1, r, 14);
+        return evalLine(r, 0, 0, 1, r+1, 15);
     }
     private int evalCol(int c) {
-        return evalLine(0, c, 1, 0, 14, c);
+        return evalLine(0, c, 1, 0, 15, c+1);
     }
     private int evalDiag(int d) {
-        return evalLine(Math.max(d, 0), Math.max(-d, 0), 1, 1, Math.min(14, d+14), Math.min(14, 14-d));
+        return evalLine(Math.max(d, 0), Math.max(-d, 0), 1, 1, Math.min(14, d+14)+1, Math.min(14, 14-d)+1);
     } 
     private int evalAntiDiag(int d) {
-        return evalLine(Math.min(d, 14), Math.max(0, d-14), -1, 1, Math.min(0, d-14), Math.max(14, d));
+        return evalLine(Math.min(d, 14), Math.max(0, d-14), -1, 1, Math.max(0, d-14)-1, Math.min(14, d)+1);
     }
     private int evalLine(int x, int y, int dx, int dy, int ex, int ey)  {
         int res = 0;
         Color color;
-        while(x <= ex && y <= ey) {
+        while(x != ex && y != ey) {
             if(cp[x][y] == null) {
                 x += dx;
                 y += dy;
@@ -73,7 +87,7 @@ public class Evaluater {
             int cnt = 0;
             color = cp[x][y].getCPColor();
             int bx = x, by = y;
-            for(; x <= ex && y <= ey && cp[x][y].getCPColor() == color; x += dx, y += dy) {
+            for(; x != ex && y != ey && cp[x][y] != null && cp[x][y].getCPColor() == color; x += dx, y += dy) {
                 cnt++;
             }
             boolean semi = true;
@@ -84,7 +98,7 @@ public class Evaluater {
             int curVal = 0;
             switch (cnt) {
                 case 1:
-                    curVal = ONE;
+                    curVal = semi ? semiONE : ONE;
                     break;
                 case 2:
                     curVal = semi ? semiTWO : TWO;
@@ -109,5 +123,17 @@ public class Evaluater {
     }
     private boolean inRange(int x) {
         return x >= 0 && x <= 14;
+    }
+    private int evalDistense(int x, int y) {
+        int decay = 0;
+        for(int i = 0; i < 15; i++) {
+            for(int j = 0; j < 15; j++) {
+                if(cp[i][j] != null) {
+                    int dis = Math.abs(i-x) + Math.abs(j-y);
+                    decay = DISTENSE_DECEY * dis;
+                }
+            }
+        }
+        return decay;
     }
 }
--- src/main/java/com/client/robot/Robot.java
+++ src/main/java/com/client/robot/Robot.java
@@ -3,18 +3,30 @@ package com.client.robot;
 import com.client.gamedata.GameData;
 import com.client.ui.ChessBoard;
 import com.client.ui.ChessPoint;
+
+import javafx.scene.effect.ColorAdjust;
+
 import java.awt.Color;
 
 
 public class Robot {
-    static int searchDepth = 3;
+    private int searchDepth = 3;
 
     private ChessPoint[][] cp = new ChessPoint[15][15];
     private Color myColor = GameData.isBlack ? Color.WHITE : Color.BLACK;
-    private ChessPoint nextPoint = new ChessPoint(8, 8, GameData.isBlack ? Color.WHITE : Color.BLACK);
+    private Color oppColor = GameData.isBlack ? Color.BLACK : Color.WHITE;
+    private ChessPoint nextPoint = new ChessPoint(7, 7, myColor);
     private Evaluater evaluater;
 
-
+    public Robot() {}
+    public Robot(int searchDepth) {
+        this.searchDepth = searchDepth;
+    }
+    public Robot(Color myColor, int searchDepth) {
+        this.myColor = myColor;
+        this.oppColor = myColor == Color.BLACK ? Color.WHITE : Color.BLACK;
+        this.searchDepth = searchDepth;
+    }
     public ChessPoint go() {
         int chessNum = 0;
         for(int i = 0; i < 15; i++) {
@@ -24,13 +36,77 @@ public class Robot {
             }
         }
         if(chessNum == 0 && myColor == Color.BLACK) {
-            return nextPoint;
+            return  new ChessPoint(7, 7, myColor);
         }
         evaluater = new Evaluater(cp, this.myColor);
-        
+        alphaBeta(searchDepth, -Evaluater.INF, Evaluater.INF, true);
         return nextPoint;
     }
-    private int alphaBeta() {
-        
-    }   
+    public ChessPoint go(ChessPoint[][] cp) {
+        int chessNum = 0;
+        for(int i = 0; i < 15; i++) {
+            for(int j = 0; j < 15; j++) {
+                this.cp[i][j] = cp[i][j];
+                if(this.cp[i][j] != null) chessNum ++;
+            }
+        }
+        if(chessNum == 0 && myColor == Color.BLACK) {
+            return new ChessPoint(7, 7, myColor);
+        }
+        evaluater = new Evaluater(this.cp, this.myColor);
+        alphaBeta(searchDepth, -Evaluater.INF, Evaluater.INF, true);
+        return nextPoint;
+    }
+    private int alphaBeta(int depth, int alpha, int beta, boolean maxPlayer) {
+        if(depth == 0) {
+            return evaluater.eval();
+        }
+        boolean terminal = true;
+        for(int i = 0; i < 15; i++) {
+            for(int j = 0; j < 15; j++) {
+                if(cp[i][j] == null)
+                    terminal = false;
+            }
+        }
+        if(terminal) {
+            return evaluater.eval();
+        }
+        if(maxPlayer) {
+            int value = -Evaluater.INF;
+            for(int i = 0; i < 15; i++) {
+                for(int j = 0; j < 15; j++) {
+                    if(cp[i][j] != null) continue;
+                    evaluater.step(i, j, myColor);
+                    int tmp = alphaBeta(depth-1, alpha, beta, false);
+                    evaluater.unstep(i, j);
+                    if(tmp > value) {
+                        value = tmp;
+                        if(depth == searchDepth) {
+                            nextPoint.setCPX(i);
+                            nextPoint.setCPY(j);
+                        }
+                    }
+                    if(value >= beta) 
+                        break;
+                    alpha = Math.max(alpha, value);
+                }
+            }
+            return value;
+        }
+        else {
+            int value = Evaluater.INF;
+            for(int i = 0; i < 15; i++) {
+                for(int j = 0; j < 15; j++) {
+                    if(cp[i][j] != null) continue;
+                    evaluater.step(i, j, oppColor);
+                    value = Math.min(value, alphaBeta(depth-1, alpha, beta, true));
+                    evaluater.unstep(i, j);
+                    if(value <= alpha) 
+                        break;
+                    beta = Math.min(beta, value);
+                }
+            }
+            return value;
+        }
+    }       
 }
